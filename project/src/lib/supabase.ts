194e7cf10631075fd784fd3ea4a6939a9f5d5a96
SlummyFurball <altomando.023@gmail.com>
2025-09-21 00:02:09 -0400

    Update supabase.ts 3

--- project/src/lib/supabase.ts
+++ project/src/lib/supabase.ts
@@ -1,7 +1,13 @@
-eimport { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+const { data, error } = await supabase
+  .from('todos')
+  .select()
 
 // Types for our database
 export interface DatabaseProperty {
