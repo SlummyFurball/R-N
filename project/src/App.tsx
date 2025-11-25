34353374b8550b57712951b118272c8efdc4eaaf
SlummyFurball <altomando.023@gmail.com>
2025-11-24 23:58:38 -0400

    Update App.tsx
    add Analytics

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Properties from './components/Properties';
@@ -37,6 +38,7 @@ function App() {
             </>
           } />
         </Routes>
+        <Analytics />
       </div>
     </Router>
   );
